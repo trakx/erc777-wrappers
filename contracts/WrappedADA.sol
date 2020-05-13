9382a0fbb3c47439b0e2c8b42cbdd085650af524
monsieurleberre <monsieurleberre@users.noreply.github.com>
2020-05-13 09:59:15 +0100

    Update contracts/WrappedADA.sol

--- contracts/WrappedADA.sol
+++ contracts/WrappedADA.sol
@@ -7,7 +7,7 @@ import "./SupervisedERC777Token.sol";
  * @title Simple777Token
  * @dev Example of instantiation of the SupervisedERC777Token
  */
-contract WrappedXRP is SupervisedERC777Token {
+contract WrappedADA is SupervisedERC777Token {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
